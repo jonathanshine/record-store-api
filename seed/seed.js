409c933ec029361e1ca5eb22c890070dbd1369d2
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-02 11:38:32 +0200

    added code to seed.js to populate the record collection with 20 fake records

--- seed/seed.js
+++ seed/seed.js
@@ -7,67 +7,94 @@ import faker from 'faker';
 
 
 (async function() {
-// MONGOOSE CONFIG ----------------------------------
-mongoose.connect("mongodb://localhost:27017/record-store-api", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-.then(() => console.log(`Connected successfully to DB`))
-.catch((err) => console.log("We cannot connect to the DB -->", err));
-// --------------------------------------------------
+    // MONGOOSE CONFIG ----------------------------------
+    mongoose.connect("mongodb://localhost:27017/record-store-api", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+    })
+    .then(() => console.log(`Connected successfully to DB`))
+    .catch((err) => console.log("We cannot connect to the DB -->", err));
+    // --------------------------------------------------
 
 
 
-// Delete all users
-try {
-    await User.deleteMany( {} );
-    console.log("All users have been deleted");
-} catch (error) {
-    console.log( error );
-};
-
-
-
-// Delete all records
-try {
-    await Record.deleteMany( {} );
-    console.log("All records have been deleted");
-} catch (error) {
-    console.log( error );
-};
-
-
-
-// Create 20 fake users
-const userPromises = Array(20)
-    .fill(null)
-    .map(() => {
-        const userData = {
-            username: faker.internet.userName(),
-            email: faker.internet.email(),
-            password: "0123456789",
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            birthday: faker.date.between("1970", "2005")
-        };
-
-        console.log(`User ${userData.username} with email ${userData.email} and password ${userData.password} has been created`);
-
-        const user = new User( userData );
-        return user.save();
-    });
-
+    // Delete all users
     try {
-        await Promise.all( userPromises );
-        console.log("----------------------------------------------");
-        console.log(`All 20 fake users have been stored to the DB`);
-        console.log("----------------------------------------------");
+        await User.deleteMany( {} );
+        console.log("All users have been deleted");
     } catch (error) {
         console.log( error );
-    }
+    };
 
-// Create 20 fake records
-// Close the connection to the DB
+
+
+    // Delete all records
+    try {
+        await Record.deleteMany( {} );
+        console.log("All records have been deleted");
+    } catch (error) {
+        console.log( error );
+    };
+
+
+
+    // Create 20 fake users
+    const userPromises = Array(20)
+        .fill(null)
+        .map(() => {
+            const userData = {
+                username: faker.internet.userName(),
+                email: faker.internet.email(),
+                password: "0123456789",
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName(),
+                birthday: faker.date.between("1970", "2005")
+            };
+
+            console.log(`User ${userData.username} with email ${userData.email} and password ${userData.password} has been created`);
+
+            const user = new User( userData );
+            return user.save();
+        });
+
+        try {
+            await Promise.all( userPromises );
+            console.log("----------------------------------------------");
+            console.log(`All 20 fake users have been stored to the DB`);
+            console.log("----------------------------------------------");
+        } catch (error) {
+            console.log( error );
+        };
+
+    // Create 20 fake records
+    const recordPromises = Array(20)
+        .fill(null)
+        .map(() => {
+            const recordData = {
+                cover: faker.image.imageUrl(),
+                title: faker.name.firstName() + " " + faker.name.jobType(),
+                artist: faker.name.lastName() + " " + faker.name.jobDescriptor(),
+                price: faker.commerce.price(10, 100, 2),
+                year: faker.date.between("1900", "2020")
+            };
+            
+            console.log(`Record ${recordData.title} by ${recordData.artist} from ${recordData.year.getFullYear()} has been created`);
+    
+            const record = new Record( recordData );
+            return record.save();
+        });
+
+
+        try {
+            await Promise.all( recordPromises );
+            console.log("----------------------------------------------");
+            console.log(`All 20 fake records have been stored to the DB`);
+            console.log("----------------------------------------------");
+        } catch (error) {
+            console.log( error );
+        };
+
+    // Close the connection to the DB
 })();
